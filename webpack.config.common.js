7cc1aad81125822b34c0fa5f41667a836f5ff03a
Dmitriy <dsilichev@gmail.com>
2025-02-27 16:06:09 +0300

    add webpack-merge

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,27 +1,30 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  entry: 'index.js',
+  context: path.resolve(__dirname, "src"),
+  entry: "./index.jsx",
   output: {
-    filename: '[name].[contenthash].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean:true
+    filename: "[name].[contenthash].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, "public/index.html"),
+  })],
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        use: 'babel-loader',
+        use: "babel-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
