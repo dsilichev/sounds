336c44958ab8109fde9da46fc25940ff32c56e8e
Dmitriy <dsilichev@gmail.com>
2025-02-28 22:54:30 +0300

    add prod plugins

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,34 @@
 const {merge} = require('webpack-merge');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const commonConfig = require('./webpack.config.common.js');
 
 module.exports = merge(commonConfig, {
-  mode: 'production',
+  mode: "production",
+  devtool: "source-map",
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static",
+      openAnalyzer: false,
+      reportFilename: "report.html",
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        vendors: false,
+        vendor: {
+          chunks: "all",
+          name: "vendor",
+          test: /node_modules/,
+        },
+      },
+      chunks: "all",
+    },
+  },
 });
