5af5f7d3c17db37581fe6851539b485e274c1af7
Dmitriy <dsilichev@gmail.com>
2025-02-28 22:45:22 +0300

    add devServer

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,4 +3,10 @@ const commonConfig = require('./webpack.config.common.js');
 
 module.exports = merge(commonConfig, {
   mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 3000,
+    hot: true,
+    open: true,
+  },
 });
