111882101fb67b489140b7e8dc121d10f7b4568a
Dmitriy <dsilichev@gmail.com>
2025-03-01 23:56:35 +0300

    add template elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
 export function App() {
-  const [count, setCount] = React.useState(0);
+
   return (
-    <>
-      <h1>Hello World!</h1>
-      <h3>Count: {count}</h3>
-      <button onClick={() => setCount((prev) => ++prev)}>Increment</button>
-    </>
+    <div className="app">
+      <div className='background'></div>
+      <div className="content">
+
+        <h1>Weather sounds</h1>
+        <div className='sounds-panel'>
+          <a href='#' className='summer'></a>
+          <a href='#' className='rain'></a>
+          <a href='#' className='winter'></a>
+        </div>
+        <div className='volume-control'>
+          <input type='range' min='0' max='100' step='1' value='50'></input>
+        </div>
+
+      </div>
+    </div>
   )
 }
