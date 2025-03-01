bf33aff0721ad6b7812bcfecf442863b7ad38b65
Dmitriy <dsilichev@gmail.com>
2025-03-02 00:16:12 +0300

    set page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
-import React from 'react';
+import React from "react";
 
 export function App() {
-
   return (
     <div className="app">
-      <div className='background'></div>
+      <div className="background"></div>
       <div className="content">
-
-        <h1>Weather sounds</h1>
-        <div className='sounds-panel'>
-          <a href='#' className='summer'></a>
-          <a href='#' className='rain'></a>
-          <a href='#' className='winter'></a>
+        <h1 className="header">Weather sounds</h1>
+        <div className="sounds-panel">
+          <a href="#" className="sounds-item summer"></a>
+          <a href="#" className="sounds-item rain"></a>
+          <a href="#" className="sounds-item winter"></a>
         </div>
-        <div className='volume-control'>
-          <input type='range' min='0' max='100' step='1' value='50'></input>
+        <div className="volume-control">
+          <input type="range" min="0" max="100" step="1" value="50"></input>
         </div>
-
       </div>
     </div>
-  )
+  );
 }
