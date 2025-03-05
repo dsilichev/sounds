9506729f971a0a62970976d624bffd4a4689d59c
Dmitriy <dsilichev@gmail.com>
2025-03-05 17:15:50 +0300

    add eslint

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,9 +1,17 @@
 const {merge} = require('webpack-merge');
+const ESLintPlugin = require('eslint-webpack-plugin');
 const commonConfig = require('./webpack.config.common.js');
 
 module.exports = merge(commonConfig, {
   mode: 'development',
   devtool: 'inline-source-map',
+  plugins: [
+    new ESLintPlugin({
+      extensions: ['js', 'jsx', 'ts', 'tsx'],
+      emitWarning: true,
+      failOnError: false,
+    }),
+  ],
   devServer: {
     port: 3000,
     hot: true,
